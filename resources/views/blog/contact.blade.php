3b2f2fa49051fa06e354fc9d62e9314b23a2a514
Adil-M2004 <D00265739@student.dkit.ie>
2025-03-27 10:30:27 +0000

    Cantact page phone number

--- resources/views/blog/contact.blade.php
+++ resources/views/blog/contact.blade.php
@@ -6,8 +6,10 @@
 
         <div class="w-4/5 m-auto pt-20">
             <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">If you have any questions or comments, please feel free to contact us. We'd love to hear from you!</p>
+            <p class="font-bold">Phone Number: 123-456-7890</p>
+
         <p class="text-gray-400">Welcome to our cooking blog! Here, we share our passion for cooking and provide you with delicious recipes, cooking tips, and culinary inspiration.
             Whether you're a seasoned chef or just starting out in the kitchen, we have something for everyone. Join us on this culinary journey and discover the joy of cooking!</p>
-    </div>
+       </div>
 </div>
 @endsection
