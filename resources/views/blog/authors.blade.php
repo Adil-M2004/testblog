0de31f2d36e0e2a178e00510b5ea3960f2ec769b
Adil-M2004 <D00265739@student.dkit.ie>
2025-03-27 12:08:19 +0000

    Home button(Link)

---
+++ resources/views/blog/authors.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-4/5 m-auto text-left">
+    <div class="py-15">
+        <h1 class="text-6xl">
+            Authors
+        </h1>
+    </div>
+</div>
+
+<div class="w-4/5 m-auto pt-20">
+    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+        Here is a list of all the authors contributing to this blog.
+    </p>
+
+    <p>Adil Manzoor</p>
+    <p>John Doe</p>
+    <p>Jane Doe</p>
+    <p>Michael Smith</p>
+    <p>Emily Johnson</p>
+    <p>Chris Brown</p>
+</div>
+@endsection
