0b1b826af5fec751a0ca432b24d584d4ee796318
Adil-M2004 <D00265739@student.dkit.ie>
2025-03-27 10:40:09 +0000

    About page Text edited

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -3,7 +3,16 @@
 @section('content')
     <div class="w-4/5 m-auto text-left">
         <h1 class="text-6xl italic text-center p-4 25px">About Us</h1>
-        <p>Welcome to our cooking blog! Here, we share our passion for cooking and provide you with delicious recipes, cooking tips, and culinary inspiration. Whether you're a seasoned chef or just starting out in the kitchen, we have something for everyone. Join us on this culinary journey and discover the joy of cooking!</p>
+
+        <div class="w-4/5 m-auto pt-20">
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">If you have any questions or comments, please feel free to contact us. We'd love to hear from you!</p>
+            <p class="font-bold p-4 20px">Phone Number: 123-456-7890</p>
+            <p class="font-bold p-4 20px">Email: email@email.com</p>
+            <p class="font-bold p-4 20px">Location: 55 Park Street, Los Angeles</p>
+
+        <p class="text-gray-400">Welcome to our cooking blog! Here, we share our passion for cooking and provide you with delicious recipes, cooking tips, and culinary inspiration.
+            Whether you're a seasoned chef or just starting out in the kitchen, we have something for everyone. Join us on this culinary journey and discover the joy of cooking!</p>
+       </div>
     </div>
 @endsection
 
