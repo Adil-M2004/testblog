0c36a9ab1201ccc7cce55a393b9ef363f12f9778
Adil-M2004 <D00265739@student.dkit.ie>
2025-03-27 10:37:25 +0000

    About Page CSS Edit

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="w-4/5 m-auto text-left">
-        <h1 class="text-6xl">About Us</h1>
+        <h1 class="text-6xl italic text-center p-4 25px">About Us</h1>
         <p>Welcome to our cooking blog! Here, we share our passion for cooking and provide you with delicious recipes, cooking tips, and culinary inspiration. Whether you're a seasoned chef or just starting out in the kitchen, we have something for everyone. Join us on this culinary journey and discover the joy of cooking!</p>
     </div>
 @endsection
